c7057d2523a79208c26121dfc3574beb1a3c89de
Alberto Pose <albertopose@gmail.com>
2014-10-27 16:40:33 -0300

    Update 03_chat_client.js

--- javascript/03_chat_client.js
+++ javascript/03_chat_client.js
@@ -2,7 +2,7 @@ return function (cb) {
   var net = require('net');
   var host = 'www.google.com';
   var client = net.connect(80, host, function () {
-    client.write('hello');
+    client.write('hello\n\n');
   });
   client.on('data', function(data) {
     client.end();
